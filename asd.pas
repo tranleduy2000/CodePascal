847bdbcd4389741621c765acb808317477034cb9
Trần Lê Duy <tranleduy1233@gmail.com>
2017-05-03 19:20:01 +0700

    learn BIT2D and BIT

--- asd.pas
+++ asd.pas
@@ -1,25 +1,25 @@
-var
-	a, b: array[1..10000] of longint;
-	f: text;
-	i, j, count, n: longint;
+
+Uses sysutils;
+
+Procedure TestIt (S1,S2 : String);
+
+Var R : Longint;
+
 begin
-	assign(f, 'file.inp');
-	reset(f);
-	readln(f, n);
-	for i  := 1 to n do read(f, a[i]);
-	for i  := 1 to n do read(f, b[i]);
-	close(f);
-	count := 0;
-	i := 1; j := 1;
-	while i <= n do
-	begin
-		while (a[i] < b[j]) and (i <= n) do inc(i);
-		while (b[j] < a[i]) and (j <= n) do inc(j);
-		if a[i] = b[j] then
-		begin
-			inc(count);
-			inc(i);
-		end;
-	end;
-	writeln(count);
-end.
+  R:=AnsiCompareStr(S1,S2);
+  Write ('"',S1,'" is ');
+  If R<0 then
+    write ('less than ')
+  else If R=0 then
+    Write ('equal to ')
+  else
+    Write ('larger than ');
+  Writeln ('"',S2,'"');
+end;
+
+Begin
+  Testit('One string','One smaller string');
+  Testit('One string','one string');
+  Testit('One string','One string');
+  Testit('One string','One tall string');
+End.
